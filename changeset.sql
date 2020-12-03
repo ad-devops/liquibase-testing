430d806446d08c1776ac2fd8fe2b6fd003cc05d5
Amit Dalal <71498261+ad-devops@users.noreply.github.com>
2020-12-03 13:28:34 +0530

    Update changeset.sql

--- changeset.sql
+++ changeset.sql
@@ -1,8 +1,9 @@
 --liquibase formatted sql
 				
 --changeset amit-dalal:1
-create table test2 (  
+create table test1 (  
     id int primary key,
-    name varchar(255)  
+    name varchar(255)
+    name test(255)
 );  
 --rollback drop table test1; 
